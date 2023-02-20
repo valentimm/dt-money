300b369fff8ec4ede9bf44be94095537327b69c8
Matheus Valentim <meduardo.valentim@gmail.com>
2023-02-20 19:43:01 -0300

    Created Header and Summary components

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { SummaryCard, SummaryContainer } from "./styles";
+import { ArrowCircleUp, ArrowCircleDown , CurrencyDollar } from "phosphor-react";
+
+export function Summary () {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+        <strong>R$ 1.259,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+        <strong>R$ 1000,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
