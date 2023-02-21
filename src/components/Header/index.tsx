57dc1f5fc13dfef7feb87024019d74568e18869c
Matheus Valentim <meduardo.valentim@gmail.com>
2023-02-21 17:27:14 -0300

    Created New Transaction modal with radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -8,8 +9,20 @@ export function Header () {
       <HeaderContainer>
         <HeaderContent>
           <img src={logoImg} alt="" />
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay />
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
 
-          <NewTransactionButton>Nova transação</NewTransactionButton>
+                <Dialog.Close />
+
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
