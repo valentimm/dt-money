6e58ad2e1140d4d9dd3ef85b71246bd707f6dd37
Matheus Valentim <meduardo.valentim@gmail.com>
2023-02-24 23:43:55 -0300

    Tested the server

--- src/pages/Tansactions/index.tsx
+++ src/pages/Tansactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -21,10 +22,13 @@ export function Transactions () {
               <tr key={transaction.id}>
               <td width="50%">{transaction.description}</td>
               <td>
-                <PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight>
+                <PriceHighlight variant={transaction.type}>
+                  {transaction.type === 'outcome' && '- '}
+                  {priceFormatter.format(transaction.price)}  
+                </PriceHighlight>
               </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
             </tr>
             )
           })}
